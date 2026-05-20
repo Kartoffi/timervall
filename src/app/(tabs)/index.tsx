6bb21aa2e0b7755dab2fb5730937eacc7df5103c
kartoffi <Stefanie.Reuter98@t-online.de>
2026-05-20 20:29:51 +0200

    Add test button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Hier gibts noch nix zu sehen c:</Text>
+      <Button label="Press me" />
       <Link href="/options" style={styles.link}>
         Go to Options
       </Link>
